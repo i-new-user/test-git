6f0ed4ded49ee146728efa9104340a7b2eb0d56b
Stanislav <gsv9026@mail.ru>
2022-11-08 15:29:06 +0300

    add array

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const  months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+
+let headerRight = document.querySelector('.header_right')
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let second = date.getSeconds();
+let nameDay = date.getDay();
